64d726e909a0dcbba3f0b3608cf60b20df4ac4e0
Sharpendx <albertomartinez@knights.ucf.edu>
2019-10-09 15:14:40 -0400

    Working into part 4

--- martinez_p1/src/problem1.java
+++ martinez_p1/src/problem1.java
@@ -8,32 +8,87 @@ public class problem1 {
     {
         SecureRandom random = new SecureRandom();
         Scanner scr = new Scanner(System.in);
-        int run = 0;
+        int lvl = 0;
         int a;
         int b;
         int response;
+        int numAnsRight = 0;
+        int numAns = 0;
+        double percentNeeded = 0.75;
+        double percentActual = 0.0;
 
-        do
+        println("Welcome to our Math learning program!");
+        println("Please select a difficulty level enter either 1, 2, or 3");
+
+        lvl = scr.nextInt();
+
+        while(lvl == 1)
         {
             response = random.nextInt(4) + 1;
             a = random.nextInt(9) + 1;
             b = random.nextInt(9) + 1;
-            GenerateQuestion(a,b,scr,response);
-        }while(run == 0);
+            numAns += GenerateQuestionLvlOne(a,b,scr,response);
+            println("You have answered " + numAns + " times.");
+            if(numAns == 10)
+            {
+                numAnsRight++;
+                percentActual = numAnsRight / numAns;
+                if(percentActual >= percentNeeded)
+                {
+                    println("Congratulations, you are ready to go to the next level!");
+                    lvl = 0;
+                }
+                else
+                {
+                    println("Please ask your teacher for extra help.");
+                    lvl = 0;
+                }
+
+            }
+            numAnsRight++;
+
+        }
+        while(lvl == 2)
+        {
+            response = random.nextInt(4) + 1;
+            a = random.nextInt(9) + 1;
+            b = random.nextInt(9) + 1;
+            numAns += GenerateQuestionLvlOne(a,b,scr,response);
+            println("You have answered " + numAns + " times.");
+            if(numAns == 10)
+            {
+                numAnsRight++;
+                percentActual = numAnsRight / numAns;
+                if(percentActual >= percentNeeded)
+                {
+                    println("Congratulations, you are ready to go to the next level!");
+                    lvl = 0;
+                }
+                else
+                {
+                    println("Please ask your teacher for extra help.");
+                    lvl = 0;
+                }
+
+            }
+            numAnsRight++;
+
+        }
 
         scr.close();
     }
-    public static void GenerateQuestion(int a, int b, Scanner scr, int response)
+    public static int GenerateQuestionLvlOne(int a, int b, Scanner scr, int response)
     {
 
         int total = a * b;
         int ans = 0;
-
+        int numAns = 0;
         println("How much is " + a + " times " + b);
 
         ans = scr.nextInt();
         if(ans == total)
         {
+            numAns++;
             switch(response)
             {
                 case 1 :
@@ -54,14 +109,14 @@ public class problem1 {
 
                 default :
                     println("ERROR WRONG NUMBER");
+                    break;
             }
-
-            //scr.close();
-            //return();
+            return(numAns);
         }
 
         while (ans != total)
         {
+            numAns++;
             switch(response)
             {
                 case 1 :
@@ -82,14 +137,14 @@ public class problem1 {
 
                 default :
                     println("ERROR WRONG NUMBER");
+                    break;
             }
 
             println("How much is " + a + " times " + b);
+
             ans = scr.nextInt();
         }
-
-        //scr.close();
-        //return();
+        return(numAns);
     }
     public static void print(String s)
     {

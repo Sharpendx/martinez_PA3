c9b3a74f2f3293f31ae6cfca2e33eae6d24e7d21
Sharpendx <albertomartinez@knights.ucf.edu>
2019-10-14 14:00:19 -0400

    working up to part 5 but division is too many decimal places

--- martinez_p1/src/problem1.java
+++ martinez_p1/src/problem1.java
@@ -1,6 +1,5 @@
-import java.util.Scanner;
 import java.security.SecureRandom;
-
+import java.util.Scanner;
 
 public class problem1 {
 
@@ -9,6 +8,7 @@ public class problem1 {
         SecureRandom random = new SecureRandom();
         Scanner scr = new Scanner(System.in);
         int lvl = 0;
+        int typeOfProb = 0;
         int a;
         int b;
         int response;
@@ -16,19 +16,61 @@ public class problem1 {
         double numAns = 0;
         double percentNeeded = 0.75;
         double percentActual = 0.0;
-        boolean ansCorrect;
+        boolean ansCorrect = false;
+        boolean randomQuest = false;
 
         println("Welcome to our Math learning program!");
-        println("Please select a difficulty level enter either 1, 2, or 3");
+        println("Please select a difficulty level enter either 1, 2, 3, or 4");
 
         lvl = scr.nextInt();
 
+        println("Please select a type of math to study:");
+        println("   Addition---------------------------1");
+        println("   Subtraction------------------------2");
+        println("   Multiplication---------------------3");
+        println("   Division---------------------------4");
+        println("   Random-----------------------------5");
+
+        typeOfProb = scr.nextInt();
+
         while(lvl == 1)
         {
+            if(typeOfProb == 5)
+            {
+                randomQuest = true;
+            }
+
+            if(randomQuest == true)
+            {
+                typeOfProb = random.nextInt(4) + 1;
+            }
+
             response = random.nextInt(4) + 1;
             a = random.nextInt(9) + 1;
             b = random.nextInt(9) + 1;
-            ansCorrect = GenerateQuestionMult(a,b,scr,response);
+            switch(typeOfProb)
+            {
+                case 1 :
+                    ansCorrect = GenerateQuestionAdd(a,b,scr,response);
+                    break;
+
+                case 2 :
+                    ansCorrect = GenerateQuestionSub(a,b,scr,response);
+                    break;
+
+                case 3 :
+                    ansCorrect = GenerateQuestionMult(a,b,scr,response);
+                    break;
+
+                case 4 :
+                    ansCorrect = GenerateQuestionDiv(a,b,scr,response);
+                    break;
+
+                default :
+                    println("ERROR WRONG NUMBER");
+                    break;
+            }
+
             if(ansCorrect == true)
             {
                 numAnsRight++;
@@ -43,8 +85,7 @@ public class problem1 {
             if(numAns == 10)
             {
                 percentActual = numAnsRight / numAns;
-                println("" + percentActual);
-                println("" + percentNeeded);
+
                 if(percentActual >= percentNeeded)
                 {
                     println("Congratulations, you are ready to go to the next level!");
@@ -57,14 +98,46 @@ public class problem1 {
                 }
 
             }
-            println("You've gotten " + numAnsRight + "so far");
+
         }
         while(lvl == 2)
         {
+            if(typeOfProb == 5)
+            {
+                randomQuest = true;
+            }
+
+            if(randomQuest == true)
+            {
+                typeOfProb = random.nextInt(4) + 1;
+            }
+
             response = random.nextInt(4) + 1;
             a = random.nextInt(99) + 1;
             b = random.nextInt(99) + 1;
-            ansCorrect = GenerateQuestionMult(a,b,scr,response);
+            switch(typeOfProb)
+            {
+                case 1 :
+                    ansCorrect = GenerateQuestionAdd(a,b,scr,response);
+                    break;
+
+                case 2 :
+                    ansCorrect = GenerateQuestionSub(a,b,scr,response);
+                    break;
+
+                case 3 :
+                    ansCorrect = GenerateQuestionMult(a,b,scr,response);
+                    break;
+
+                case 4 :
+                    ansCorrect = GenerateQuestionDiv(a,b,scr,response);
+                    break;
+
+                default :
+                    println("ERROR WRONG NUMBER");
+                    break;
+            }
+
             if(ansCorrect == true)
             {
                 numAnsRight++;
@@ -75,10 +148,11 @@ public class problem1 {
             {
                 numAns++;
             }
-            //println("You have answered " + numAns + " times.");
+
             if(numAns == 10)
             {
                 percentActual = numAnsRight / numAns;
+
                 if(percentActual >= percentNeeded)
                 {
                     println("Congratulations, you are ready to go to the next level!");
@@ -91,6 +165,141 @@ public class problem1 {
                 }
 
             }
+
+        }
+        while(lvl == 3)
+        {
+            if(typeOfProb == 5)
+            {
+                randomQuest = true;
+            }
+
+            if(randomQuest == true)
+            {
+                typeOfProb = random.nextInt(4) + 1;
+            }
+
+            response = random.nextInt(4) + 1;
+            a = random.nextInt(999) + 1;
+            b = random.nextInt(999) + 1;
+            switch(typeOfProb)
+            {
+                case 1 :
+                    ansCorrect = GenerateQuestionAdd(a,b,scr,response);
+                    break;
+
+                case 2 :
+                    ansCorrect = GenerateQuestionSub(a,b,scr,response);
+                    break;
+
+                case 3 :
+                    ansCorrect = GenerateQuestionMult(a,b,scr,response);
+                    break;
+
+                case 4 :
+                    ansCorrect = GenerateQuestionDiv(a,b,scr,response);
+                    break;
+
+                default :
+                    println("ERROR WRONG NUMBER");
+                    break;
+            }
+
+            if(ansCorrect == true)
+            {
+                numAnsRight++;
+                numAns++;
+            }
+
+            if(ansCorrect != true)
+            {
+                numAns++;
+            }
+
+            if(numAns == 10)
+            {
+                percentActual = numAnsRight / numAns;
+
+                if(percentActual >= percentNeeded)
+                {
+                    println("Congratulations, you are ready to go to the next level!");
+                    lvl = 0;
+                }
+                else
+                {
+                    println("Please ask your teacher for extra help.");
+                    lvl = 0;
+                }
+
+            }
+
+        }
+        while(lvl == 4)
+        {
+            if(typeOfProb == 5)
+            {
+                randomQuest = true;
+            }
+
+            if(randomQuest == true)
+            {
+                typeOfProb = random.nextInt(4) + 1;
+            }
+
+            response = random.nextInt(4) + 1;
+            a = random.nextInt(9999) + 1;
+            b = random.nextInt(9999) + 1;
+            switch(typeOfProb)
+            {
+                case 1 :
+                    ansCorrect = GenerateQuestionAdd(a,b,scr,response);
+                    break;
+
+                case 2 :
+                    ansCorrect = GenerateQuestionSub(a,b,scr,response);
+                    break;
+
+                case 3 :
+                    ansCorrect = GenerateQuestionMult(a,b,scr,response);
+                    break;
+
+                case 4 :
+                    ansCorrect = GenerateQuestionDiv(a,b,scr,response);
+                    break;
+
+                default :
+                    println("ERROR WRONG NUMBER");
+                    break;
+            }
+
+            if(ansCorrect == true)
+            {
+                numAnsRight++;
+                numAns++;
+            }
+
+            if(ansCorrect != true)
+            {
+                numAns++;
+            }
+
+            if(numAns == 10)
+            {
+                percentActual = numAnsRight / numAns;
+
+                if(percentActual >= percentNeeded)
+                {
+                    println("Congratulations, you are ready to go to the next level!");
+                    lvl = 0;
+                }
+                else
+                {
+                    println("Please ask your teacher for extra help.");
+                    lvl = 0;
+                }
+
+            }
+
         }
         scr.close();
     }
@@ -103,6 +312,205 @@ public class problem1 {
         println("How much is " + a + " times " + b);
 
         ans = scr.nextInt();
+        if(ans == total)
+        {
+            numAns++;
+            switch(response)
+            {
+                case 1 :
+                    println("Very good!");
+                    break;
+
+                case 2 :
+                    println("Excellent!");
+                    break;
+
+                case 3 :
+                    println("Nice work!");
+                    break;
+
+                case 4 :
+                    println("Keep up the good work!");
+                    break;
+
+                default :
+                    println("ERROR WRONG NUMBER");
+                    break;
+            }
+            return(true);
+        }
+
+        if (ans != total)
+        {
+            numAns++;
+            switch(response)
+            {
+                case 1 :
+                    println("No. Please try again.");
+                    break;
+
+                case 2 :
+                    println("Wrong. Try once more.");
+                    break;
+
+                case 3 :
+                    println("Don’t give up!");
+                    break;
+
+                case 4 :
+                    println("No. Keep trying.");
+                    break;
+
+                default :
+                    println("ERROR WRONG NUMBER");
+                    break;
+            }
+
+        }
+        return(false);
+    }
+    public static boolean GenerateQuestionAdd(int a, int b, Scanner scr, int response)
+    {
+
+        int total = a + b;
+        int ans = 0;
+        int numAns = 0;
+        println("How much is " + a + " plus " + b);
+
+        ans = scr.nextInt();
+        if(ans == total)
+        {
+            numAns++;
+            switch(response)
+            {
+                case 1 :
+                    println("Very good!");
+                    break;
+
+                case 2 :
+                    println("Excellent!");
+                    break;
+
+                case 3 :
+                    println("Nice work!");
+                    break;
+
+                case 4 :
+                    println("Keep up the good work!");
+                    break;
+
+                default :
+                    println("ERROR WRONG NUMBER");
+                    break;
+            }
+            return(true);
+        }
+
+        if (ans != total)
+        {
+            numAns++;
+            switch(response)
+            {
+                case 1 :
+                    println("No. Please try again.");
+                    break;
+
+                case 2 :
+                    println("Wrong. Try once more.");
+                    break;
+
+                case 3 :
+                    println("Don’t give up!");
+                    break;
+
+                case 4 :
+                    println("No. Keep trying.");
+                    break;
+
+                default :
+                    println("ERROR WRONG NUMBER");
+                    break;
+            }
+
+        }
+        return(false);
+    }
+    public static boolean GenerateQuestionSub(int a, int b, Scanner scr, int response)
+    {
+
+        int total = a - b;
+        int ans = 0;
+        int numAns = 0;
+        println("How much is " + a + " minus " + b);
+
+        ans = scr.nextInt();
+        if(ans == total)
+        {
+            numAns++;
+            switch(response)
+            {
+                case 1 :
+                    println("Very good!");
+                    break;
+
+                case 2 :
+                    println("Excellent!");
+                    break;
+
+                case 3 :
+                    println("Nice work!");
+                    break;
+
+                case 4 :
+                    println("Keep up the good work!");
+                    break;
+
+                default :
+                    println("ERROR WRONG NUMBER");
+                    break;
+            }
+            return(true);
+        }
+
+        if (ans != total)
+        {
+            numAns++;
+            switch(response)
+            {
+                case 1 :
+                    println("No. Please try again.");
+                    break;
+
+                case 2 :
+                    println("Wrong. Try once more.");
+                    break;
+
+                case 3 :
+                    println("Don’t give up!");
+                    break;
+
+                case 4 :
+                    println("No. Keep trying.");
+                    break;
+
+                default :
+                    println("ERROR WRONG NUMBER");
+                    break;
+            }
+
+        }
+        return(false);
+    }
+    public static boolean GenerateQuestionDiv(int a, int b, Scanner scr, int response)
+    {
+        double aD = a;
+        double bD = b;
+        double total = a / b;
+        double ans = 0;
+        int numAns = 0;
+        println("How much is " + a + " divided by " + b);
+
+        ans = scr.nextDouble();
         if(ans == total)
         {
             numAns++;
